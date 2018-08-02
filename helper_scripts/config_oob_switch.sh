65c7f2c94aa4367d8324a3dacfe0d3cbdf40d8a6
JR Rivers <jrrivers@cumulusnetworks.com>
2018-08-01 17:07:47 -0700

    update oob_switch config... nclu doesn't do out of bound interfaces

--- helper_scripts/config_oob_switch.sh
+++ helper_scripts/config_oob_switch.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
+MAXIF=$(ls -d /sys/class/net/swp* | sed "s/^.*swp//" | sort -g | tail -n 1)
+
 net add vlan 1
-net add interface swp1-48 bridge access 1
+net add interface swp1-${MAXIF} bridge access 1
 net add vlan 1 ip address 192.168.0.1/24
 net commit
 
